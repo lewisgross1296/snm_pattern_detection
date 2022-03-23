215fe3c6a98ac50b4f9804569140dab2c6a325ce
Lewis <ligross@wisc.edu>
2022-03-22 22:49:41 -0500

    took out MCNP from plotting, to do write these into a file in CSV format instead

--- steel/snm_plot_simulation.py
+++ steel/snm_plot_simulation.py
@@ -8,18 +8,10 @@ import PyFrensie.Utility as Utility
 import PyFrensie.MonteCarlo as MonteCarlo
 import PyFrensie.MonteCarlo.Event as Event
 import PyFrensie.MonteCarlo.Manager as Manager
-from PyFrensie.spectrum_plot_tools import plotSpectralDataWithErrors
-from MCNP_data_extractor import extractData
 
 def plotSNMSimulationSpectrum( rendezvous_file,
                                   estimator_id,
-                                  entity_id,
-                                  mcnp_file,
-                                  mcnp_file_start,
-                                  top_ylims = None,
-                                  bottom_ylims = None,
-                                  xlims = None,
-                                  legend_pos = None ):
+                                  entity_id):
 
     # Activate just-in-time initialization to prevent automatic loading of the
     # geometry and data tables
@@ -33,37 +25,7 @@ def plotSNMSimulationSpectrum( rendezvous_file,
 
     entity_bin_data = estimator.getEntityBinProcessedData( entity_id )
     entity_bin_data["e_bins"] = estimator.getTimeDiscretization()
-    #TODO ASK IF THIS IS CORRECT, "e_bins" meaning?
 
-    # print FRENSIE results
+    # TODO print FRENSIE results to CSV forrmat
     for i in range(0,len(entity_bin_data["mean"])):
         print entity_bin_data["e_bins"][i+1], entity_bin_data["mean"][i], entity_bin_data["re"][i]
-    
-    # create a dictionary for the mcnp data
-    # TODO ASK ABOUT TIME VS ENERGY
-    mcnp_bin_data = {"e_up": [], "mean": [], "re": []}
-    
-    # Extract the mcnp data from the output file
-    time , mean , re = extractData( mcnp_file_start, mcnp_file)
-
-    for i in range(0,len(time)):
-        mcnp_bin_data["e_up"].append( float(time[i]) )
-        mcnp_bin_data["mean"].append( float(mean[i]) )
-        mcnp_bin_data["re"].append( float( re[i] ) )
-
-    print "MCNP"
-    print mcnp_bin_data
-
-    # Plot the data
-    plotSpectralDataWithErrors( "FRENSIE",
-                                entity_bin_data,
-                                "MCNP6",
-                                mcnp_bin_data,
-                                "Flux",
-                                False,
-                                False,
-                                top_ylims = top_ylims,
-                                bottom_ylims = bottom_ylims,
-                                xlims = xlims,
-                                legend_pos = legend_pos,
-                                output_plot_names = ['air_empty_25.eps'] )
--- steel/steel_25_0_-25/snm-plot.py
+++ steel/steel_25_0_-25/snm-plot.py
@@ -14,25 +14,9 @@ if __name__ == "__main__":
                       help="the estimator id to use")
     parser.add_option("--entity_id", type="int", dest="entity_id",
                       help="the entity id to use")
-    parser.add_option("--mcnp_file", type="string", dest="mcnp_file",
-                      help="the mcnp output file to load")
-    parser.add_option("--mcnp_file_start", type="int", dest="mcnp_file_start",
-                      help="the mcnp output file start line")
     options,args = parser.parse_args()
 
-    # adjust input for axes on each image
-    top_ylims = [0.0, 1e-4]
-    bottom_ylims = [0.95, 1.05]
-    # legend position
-    legend_pos = (0.95,0.95)
-        
     # Plot the spectrum
     plotSNMSimulationSpectrum( options.rendezvous_file,
                                   options.estimator_id,
-                                  options.entity_id,
-                                  options.mcnp_file,
-                                  options.mcnp_file_start,
-                                  top_ylims = top_ylims,
-                                  bottom_ylims = bottom_ylims,
-                                  xlims = [0,0.2],
-                                  legend_pos = legend_pos )
+                                  options.entity_id)
