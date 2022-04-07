4bdac75001bba59a8becae4e5a8b7c030fdcd602
Lewis <ligross@wisc.edu>
2022-04-07 13:32:38 -0500

    changed submit scripts so that they will run (-25,-25,-25) on pre. initial RV and out file

--- steel/steel_-25_-25_-25/snm-restart-submit.sh
+++ steel/steel_-25_-25_-25/snm-restart-submit.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-#SBATCH --partition=cnerg
-#SBATCH --time=7-00:00:00 
-#SBATCH --nodes=8
+#SBATCH --partition=pre
+#SBATCH --time=1-00:00:00 
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=5
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=120000
 #SBATCH --error=snm_%J.err
 #SBATCH --output=snm_%J.out
 #SBATCH --constrain=avx2
-
 
 # build command
 # FIRST LOG INTO AN INTERACTIVE NODE: srun -n1 -N1 -p int --pty bash
