a6ed97c9d868c82910da953434952a1e1919c6d2
Lewis <ligross@wisc.edu>
2022-05-26 15:20:52 -0500

    files necessary for starting a simulation at the Nth history

---
+++ steel/steel_25_0_-25/snm-start-N.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import sys, os
+from optparse import *
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from snm_simulation import snmSimulation
+import PyFrensie.Utility as Utility
+
+
+if __name__ == "__main__":
+
+        # Parse the command line options
+    parser = OptionParser()
+    parser.add_option("--threads", type="int", dest="threads", default=1,
+                      help="the number of threads to use")
+    parser.add_option("--db_path", type="string", dest="db_path",
+                      help="the database name (with extension)")
+    parser.add_option("--sim_name", type="string", dest="sim_name", default="snm",
+                      help="the simulation name")
+    parser.add_option("--log_file", type="string", dest="log_file",
+                      help="the file that will be used for logging")
+    parser.add_option("--num_particles", type="float", dest="num_particles", default=1e3,
+                      help="the number of particles to run")
+    parser.add_option("--start_history", type="float", dest="start_history", default=0,
+                      help="the initial history number")
+    parser.add_option("--source_energy", type="float", dest="source_energy",default=14.1,
+                      help="the source energy in MeV")
+    options,args = parser.parse_args()
+
+    if options.db_path is None:
+        print "The database path must be specified!"
+        sys.exit(1)
+
+    # Run the simulation
+    snmSimulation( options.sim_name,
+                   options.db_path,
+                   options.num_particles,
+                   options.start_history,
+                   options.source_energy,
+                   options.threads,
+                   options.log_file )
---
+++ steel/steel_25_0_-25/snm-start-at-N.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --partition=pre
+#SBATCH --time=1-00:00:00 
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=5
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=128000
+#SBATCH --error=snm_%J.err
+#SBATCH --output=snm_%J.out
+#SBATCH --constrain=avx2
+
+
+# build command
+# FIRST LOG INTO AN INTERACTIVE NODE: srun -n1 -N1 -p int --pty bash
+# may possibly need to unset the two below environemnt variables
+# unset HTTPS_PROXY
+# unset http_proxy
+
+# BUILD SINGULARITY IMAGE FROM DOCKERHUB IMAGE
+# singularity build frensie_hpc_at_N.simg docker://ligross/frensie_start_n:frensie_start_n
+
+#BIND path to shared data in dagmc group
+BIND_PATH=/software/groups/dagmc/opt/misc/MCNP/MCNP_DATA
+module load openmpi
+mpirun -np $SLURM_NTASKS singularity exec --bind ${BIND_PATH}:${BIND_PATH} frensie_hpc_at_N.simg python snm-start-N.py --sim_name="snm_start_5e10" --num_particles=1e12 --threads=$SLURM_CPUS_PER_TASK --db_path=${BIND_PATH}/database.xml
+ --start_history=5e10
